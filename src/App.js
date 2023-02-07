5fb203d315d72280a539d53f07fb6e173cdca492
Adam Ledwon <ledwon.adam1989@gmail.com>
2023-02-07 16:15:57 +0100

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -31,18 +31,29 @@ function App() {
 
       return task;
     }));
-  }
+  };
 
   const setAllDone = () => {
     setTasks (tasks => tasks.map(task=>({
       ...task,
       done: true,
-    })))
-  }
+    })));
+  };
+
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content: content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id +1 : 1,
+      },
+    ]);
+  };
 
   return (
     <Container title="Lista zadań">
-      <Form title="Dodaj nowe zadanie"/>
+      <Form addNewTask={addNewTask}/>
       <Section title="Lista zadań" 
       extraContent={
         <Buttons 
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,15 +1,32 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = ({ title }) => (
-    <section className="form">
-        <form>
-            <h2 className="form__header">{title}</h2>
-            <div className="form__container">
-                <input className="form__input" placeholder=" Co jest do zrobienia?" autoFocus />
-                <button className="form__button">Dodaj zadanie</button>
-            </div>
-        </form>
-    </section>
-);
+const Form = ({ addNewTask }) => {
+    const [newTaskContent, setNewTaskContent] = useState ("");
+    
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        addNewTask(newTaskContent.trim());
+        setNewTaskContent("");
+    };
+
+    return (
+        (
+            <section className="form">
+                <form onSubmit={onFormSubmit}>
+                    <h2 className="form__header">Dodaj nowe zadanie</h2>
+                    <div className="form__container">
+                        <input 
+                        value={newTaskContent}
+                        className="form__input" 
+                        placeholder=" Co jest do zrobienia?" 
+                        onChange={({ target }) => setNewTaskContent(target.value)} />
+                        <button className="form__button">Dodaj zadanie</button>
+                    </div>
+                </form>
+            </section>
+        )
+    )
+};
 
 export default Form;
